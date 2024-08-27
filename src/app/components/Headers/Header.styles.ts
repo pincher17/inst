13bca76f669d3ec98dafa6aca8f9e22f7f500737
Andrei P <pinchers17@mail.ru>
2024-08-27 21:16:10 +0300

    add Headers

---
+++ src/app/components/Headers/Header.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Headers = styled('header')`
+  width: 100%;
+  height: 60px;
+  position: fixed;
+  margin-bottom: 50px;
+  top: 0;
+  z-index: 5;
+  left: 0;
+ /*  -webkit-backdrop-filter: blur(111px);
+    backdrop-filter: blur(111px); */
+   /*  background: linear-gradient(180deg,#f8fcff00,rgb(248 252 255 / 32%)); */
+    /* box-shadow: 0 1rem 5.5rem rgba(23,39,80,.3); */
+`
---
+++ src/app/components/Headers/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Headers } from './Header.styles'
+
+const Header: React.FC = () => {
+
+
+
+  return (
+        <Headers>
+
+        </Headers>
+  )
+}
+
+export default Header
